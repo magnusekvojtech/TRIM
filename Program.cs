f942b7ee811b75620f3e7a25229afc19b45c8aa3
√ｲﾶﾑム <113216701+magnusekvojtech@users.noreply.github.com>
2023-02-23 22:46:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Zadejte číslo:");
+string retezec = Console.ReadLine();
+Console.WriteLine("Zadal jste text:" + retezec);
+Console.WriteLine("Text pro funkci trim:" + retezec.Trim());
+int a = int.Parse(retezec);
+Console.WriteLine("Převedl jsem zadaný text na číslo parsováním, zadal jste:" + a);
+Console.ReadKey();
+
+
+
